f57ef310446b186e91b8dd97167272159dafcf6a
Chathurya Sudhakarreddy <chathurya901@gmail.com>
2025-10-17 11:01:26 -0500

    Updating sprint 2 front end

--- jobs/urls.py
+++ jobs/urls.py
@@ -6,11 +6,12 @@ app_name = "jobs"
 urlpatterns = [
     path("", views.home, name="home"),
     path("signup/", views.signup, name="signup"),
+    path("upload_resume/", views.upload_resume, name="upload_resume"),
 
-    # New routes for bookmarks & applications
+    # Bookmarks & applications
     path("bookmark/<int:job_id>/", views.bookmark_job, name="bookmark_job"),
     path("bookmarks/", views.my_bookmarks, name="my_bookmarks"),
-
     path("apply/<int:job_id>/", views.apply_job, name="apply_job"),
     path("applications/", views.my_applications, name="my_applications"),
 ]
+
