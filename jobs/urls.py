941bff8b69155789c03bca4a9fcd0766d9a6f60e
Nahian1504 <nahian.tasnim@slu.edu>
2025-11-08 16:43:57 +0600

    Updating suggestions url

---
+++ jobs/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+app_name = "jobs"
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("signup/", views.signup, name="signup"),
+    path("upload_resume/", views.upload_resume, name="upload_resume"),
+    path("suggestions/", views.ai_job_suggestions, name="ai_suggestions"),
+
+    # Bookmarks & applications
+    path("bookmark/<int:job_id>/", views.bookmark_job, name="bookmark_job"),
+    path("bookmarks/", views.my_bookmarks, name="my_bookmarks"),
+    path("apply/<int:job_id>/", views.apply_job, name="apply_job"),
+    path("applications/", views.my_applications, name="my_applications"),
+]
+
